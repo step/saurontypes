bf80e20099ebbd29a599897c91b10f7ed70065f1
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2019-06-30 17:16:13 +0530

    Made UrukMessage a Stringer
    Also moved angmarMessage.go to messages.go

--- messages.go
+++ messages.go
@@ -5,10 +5,11 @@ import "strings"
 // AngmarMessage is a struct that encapsulates the message that Angmar
 // listens to on a queue for.
 type AngmarMessage struct {
-	Url    string
-	SHA    string
-	Pusher string
-	Tasks  []string
+	Url     string
+	SHA     string
+	Pusher  string
+	Project string
+	Tasks   []string
 }
 
 // String returns a stringified version of AngmarMessage, but doesn't
@@ -16,6 +17,7 @@ type AngmarMessage struct {
 func (m AngmarMessage) String() string {
 	var builder strings.Builder
 	builder.WriteString("URL: " + m.Url + "\n")
+	builder.WriteString("Project: " + m.Project + "\n")
 	builder.WriteString("SHA: " + m.SHA + "\n")
 	builder.WriteString("Pusher: " + m.Pusher + "\n")
 	return builder.String()
@@ -26,4 +28,12 @@ func (m AngmarMessage) String() string {
 type UrukMessage struct {
 	ImageName    string
 	RepoLocation string
+}
+
+// String returns a stringified version of UrukMessage
+func (m UrukMessage) String() string {
+	var builder strings.Builder
+	builder.WriteString("Image: " + m.ImageName + "\n")
+	builder.WriteString("Repo Location: " + m.RepoLocation + "\n")
+	return builder.String()
 }
